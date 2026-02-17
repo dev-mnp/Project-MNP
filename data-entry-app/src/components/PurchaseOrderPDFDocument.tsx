063da8761eb44424b2ca45233653652e94b98e55
ash-sha <aswathshakthi@outlook.com>
2026-02-18 03:28:59 +0530

    formatting

--- data-entry-app/src/components/PurchaseOrderPDFDocument.tsx
+++ data-entry-app/src/components/PurchaseOrderPDFDocument.tsx
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
         objectFit: 'contain',
    },
   poTitle: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
     color: '#008000',
     marginBottom: 5,
